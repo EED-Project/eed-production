6df34a8f94ff503d0dd375c4206366b8cdc05e72
emerginginsights <65678311+emerginginsights@users.noreply.github.com>
2020-11-16 19:00:06 -0400

    Revert "Link wiht page"
    This reverts commit aa3b64a191bb3310fadbdf02616aa2b9ed561f16.

--- homepage/js/app.js
+++ homepage/js/app.js
@@ -18,7 +18,6 @@ function suggestionElementClickHandler(e) {
 
     const sugValue = e.target.dataset.sug;
     searchInputElement.val(sugValue);
-    window.location.replace("https://eed-app.herokuapp.com/?country="+sugValue);
 }
 
 function searchInputElementInputHandler(e) {

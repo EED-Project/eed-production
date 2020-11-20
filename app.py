94e5fa559b870a8487302a6c25c1ddcecf8f0cb3
emerginginsights <65678311+emerginginsights@users.noreply.github.com>
2020-11-20 11:07:50 -0400

    Updating

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import os
 
 from backend import create_app
@@ -12,3 +13,19 @@ if __name__ == '__main__':
     #port = int(os.environ.get('PORT', 8000))
     #app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
+=======
+import os
+
+from backend import create_app
+
+homepage_folder_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), 'frontend')
+
+
+app = create_app(static_folder=homepage_folder_path, template_folder=homepage_folder_path, static_url_path='')
+
+if __name__ == '__main__':
+    #port = int(os.environ.get('PORT', 8000))
+    #app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+>>>>>>> Stashed changes
--- backend/__init__.py
+++ backend/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import os
 
 from flask import Flask, jsonify, send_from_directory, send_file, render_template, redirect, url_for, request
@@ -51,3 +52,58 @@ def create_app(**kwargs):
         '''
 
     return app
+=======
+import os
+
+from flask import Flask, jsonify, send_from_directory, send_file, render_template, redirect, url_for, request
+
+from backend.admin import setup_admin
+from backend.api import stats_api_bp
+from backend.models import db
+
+app = Flask(__name__)
+
+def create_app(**kwargs):
+    app = Flask(__name__, **kwargs)
+    app.config.from_pyfile("config.cfg")
+
+    # Configure by environment variables
+    if os.getenv("EED_DB_NAME", None) or os.getenv("EED_DB_HOST", None):
+        db_name = os.getenv("EED_DB_NAME", "df9egclbrc07pm")
+        db_host = os.getenv("EED_DB_HOST", "ec2-54-160-202-3.compute-1.amazonaws.com")
+        db_user = os.getenv("EED_DB_USER", "ompjvgphfcccsl")
+        db_pass = os.getenv("EED_DB_PASS", "12aac0929be8a501efc293dda0bb31d3c3263d0a267704ac8a5a852eab637564")
+        app.config[
+            "SQLALCHEMY_DATABASE_URI"
+        ] = f"postgresql://{db_user}:{db_pass}@{db_host}/{db_name}"
+
+    db.init_app(app)
+
+    app.register_blueprint(stats_api_bp)
+
+    setup_admin(app, db)
+
+    @app.route("/ping")
+    def ping_pong():
+        return jsonify("pong")
+
+    @app.route("/")
+    def index():
+        if request.args.get("country") is not None:
+            return render_template('dashboard.html')
+        else:
+            return render_template("index.html")
+        #return render_template('index.html')
+
+
+    '''
+    @app.route("/dashboard/", methods=['GET', 'POST'])
+    def dashboard():
+        a = request.args.get("country")
+        #return app.send_static_file("dashboard.html")
+        #return redirect(url_for('static', filename='dashboard.html/?country=Angola'))
+        return render_template('dashboard.html')
+        '''
+
+    return app
+>>>>>>> Stashed changes

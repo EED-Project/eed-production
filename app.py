bb4df1627046c5ccce784337f474adaf7f0c5bed
emerginginsights <65678311+emerginginsights@users.noreply.github.com>
2020-11-20 11:33:08 -0400

    Updating from Fiverr

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import os
 
 from backend import create_app
@@ -10,22 +9,5 @@ homepage_folder_path = os.path.join(
 app = create_app(static_folder=homepage_folder_path, template_folder=homepage_folder_path, static_url_path='')
 
 if __name__ == '__main__':
-    #port = int(os.environ.get('PORT', 8000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
-=======
-import os
-
-from backend import create_app
-
-homepage_folder_path = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), 'frontend')
-
-
-app = create_app(static_folder=homepage_folder_path, template_folder=homepage_folder_path, static_url_path='')
-
-if __name__ == '__main__':
-    #port = int(os.environ.get('PORT', 8000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
->>>>>>> Stashed changes
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
